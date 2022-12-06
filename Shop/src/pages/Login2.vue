e528b82702f38961f12117547bfd6b7abc952982
Elizabevil <2197377595@qq.com>
2022-12-06 02:25:57 -0800

    init

--- Shop/src/pages/Login2.vue
+++ Shop/src/pages/Login2.vue
@@ -1,8 +1,8 @@
 <template lang="pug">
 .box
   .login_background
-  div.Login_Title Sign In
 
+  div.Login_Title Sign In
   div.Login_form
     div
       ul
@@ -12,14 +12,10 @@
         li
           .form_title Password
           input
-    div
+    div.remember_me
+      input(type="checkbox")
       div  Remember me
-      div.newlogindialog_Checkbox(tabindex="0")
-      svg.SVGIcon_Button.SVGIcon_Check(version='1.1', xmlns='http://www.w3.org/2000/svg', x='0px', y='0px', width='256px', height='256px', viewbox='0 0 256 256', stroke-width='35', stroke='#fff', stroke-linecap='round', stroke-linejoin='round', stroke-miterlimit='10')
-        polyline(fill='none', points='49.5,147.75 95,210.75 206.5,45.25 ')
-
-    button 登录
-
+    button Sign In
 </template>
 
 <script lang="ts" setup>
@@ -33,6 +29,7 @@
   width: 100vw
   height: 100vh
   display: flex
+  color: #03e9f4
   background-clip: border-box
   flex-direction: column
   flex-wrap: nowrap
@@ -40,17 +37,24 @@
   align-items: center
   justify-content: center
 
+
 .login_background
   position: absolute
   width: 100%
   height: 100%
   background: url("src/assets/login_back.jpg") no-repeat 100% 100%
   -webkit-background-size: cover
+  -moz-background-size: cover
+  -o-background-size: cover
+  background-size: cover
+  z-index: -1
+
 .Login_Title
-  color: #fff
+  color: #ffffff
   font-size: 32px
+  margin-bottom: 20px
+  margin-right: 400px
   text-transform: uppercase
-  letter-spacing: .055em
   font-weight: 200
 
 .Login_form
@@ -58,7 +62,7 @@
   position: relative
   background: #000000
   border: 1px solid #000
-  border-radius: 10%
+  border-radius: 5%
   display: flex
   flex-direction: column
   align-items: center
@@ -75,26 +79,28 @@ ul
 ul li
   margin-top: calc($font_size / 2)
 
-input
-  margin-top: calc($font_size / 2)
-  border-radius: 5px
-  padding: calc($font_size / 2)
-  box-sizing: border-box
-  font-size: $font_size
-  font-weight: 400
-  background: rgb(208, 208, 208)
+  input
+    margin-top: calc($font_size / 2)
+    border-radius: 5px
+    padding: calc($font_size / 2)
+    box-sizing: border-box
+    font-size: $font_size
+    font-weight: 400
+    background: rgb(208, 208, 208)
 
   &:focus
     outline: none
 
+.remember_me
+  padding: 10px
+  text-align: left
+  display: flex
+  flex-direction: row
+  align-items: center
+  margin-right: auto
 
-.newlogindialog_Checkbox
-  width: 14px
-  height: 14px
-  padding: 3px
-  border-radius: 2px
-  background-color: #32353c
-  cursor: pointer
+  div
+    margin-left: 5px
 
 
 button
@@ -103,7 +109,7 @@ button
   border-radius: 2px
   border: none
   outline: none
-  padding: 12px
+  padding: 8px
   color: #fff
   font-size: 18px
   font-weight: 500
